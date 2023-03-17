2ea9806f75a2648f4e92b09cb1c3cae72148d2f0
“sochoac1” <sochoac1@eafit.edu.co>
2023-03-16 21:23:07 -0500

    Adding urls from user and mainApp to the project

--- fenix/fenix/settings.py
+++ fenix/fenix/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainApp',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+AUTH_USER_MODEL = 'user.User'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
