c4986b14d903ad8e89057eaa8504ba375c902494
dgomezc1 <dgomezc10@eafit.edu.co>
2023-03-25 12:55:28 -0500

    Login and logout controller

--- fenix/accounts/urls.py
+++ fenix/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from projectowner.views import create_project_owner
 from investor.views import create_investor
+from .views import loginaccount, logoutaccount
 from . import views
 
 urlpatterns = [
     path('project_owner/create', create_project_owner, name='project_owner.singup'),
     path('investor/create', create_investor, name='investor.singup'),
+    path('login', loginaccount, name='user.login'),
+    path('logout', logoutaccount, name='user.logout'),
 ]
--- fenix/accounts/views.py
+++ fenix/accounts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import redirect
+from django.db import IntegrityError
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def logoutaccount(request):        
+    logout(request)
+    return redirect('user.login')
+
+def loginaccount(request):    
+    if request.method == 'GET':
+        return render(request, 'loginaccount.html', 
+                      {'form':AuthenticationForm})
+    else:
+        user = authenticate(request, username=request.POST['username'],
+                            password=request.POST['password'])
+        if user is None:
+            return render(request,'loginaccount.html', 
+                    {'form': AuthenticationForm(), 
+                    'error': 'username and password do not match'})
+        else: 
+            login(request,user)
+            return redirect('home')
