1d2d5488601dbe68186d84b8dce302e0a4d11194
MiguelZapata04 <mazapataj@eafit.edu.co>
2023-03-29 12:32:57 -0500

    Improve forms styles

--- fenix/project/urls.py
+++ fenix/project/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='projects'),
-    path('<int:project_id>', views.detail, name='projects.detail'),
-    path('create', views.create_project, name='projects.create_project'),
+    path('<int:project_id>/', views.detail, name='projects.detail'),
+    path('create/', views.create_project, name='projects.create_project'),
 ]
