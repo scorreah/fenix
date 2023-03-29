1e0617bdecffa1c9e04994e7857d5cda573f6784
sochoac1 <sochoac1@eafit.edu.co>
2023-03-28 23:13:55 -0500

    Adding links, fixing style and stylying cards

--- fenix/project/urls.py
+++ fenix/project/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='projects.index'),
+    path('', views.home, name='projects'),
     path('<int:project_id>', views.detail, name='projects.detail'),
     path('create', views.create_project, name='projects.create_project'),
 ]
