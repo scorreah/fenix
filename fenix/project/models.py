a095be2682b5e54064eaa3673f2c93508f363db9
sochoac1 <sochoac1@eafit.edu.co>
2023-04-25 19:54:18 -0500

    Begining filter

--- fenix/project/models.py
+++ fenix/project/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 from projectowner.models import ProjectOwner
 from investor.models import Investor
-
+#hola
 class Project(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
