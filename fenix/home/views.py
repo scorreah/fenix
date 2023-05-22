312319a560f858466c81fd37fbf14ca2876b69cf
MiguelZapata04 <mazapataj@eafit.edu.co>
2023-05-21 20:55:37 -0500

    Adding icons

--- fenix/home/views.py
+++ fenix/home/views.py
@@ -9,6 +9,12 @@ def home(request):
     total_projects = Project.objects.all().count()
     global_amount_invested = Investing.objects.all().aggregate(Sum('amount'))
     investments = Investing.objects.all()
+    technology_projects = Project.objects.filter(category="TEC").count()
+    science_projects = Project.objects.filter(category="SCI").count()
+    art_projects = Project.objects.filter(category="ART").count()
+    reading_projects = Project.objects.filter(category="REA").count()
+    health_projects = Project.objects.filter(category="HEA").count()
+    music_projects = Project.objects.filter(category="MUS").count()
     repeat_investors = []
     investors = []
     for i in investments:
@@ -19,5 +25,5 @@ def home(request):
             if i['investor'] == j['investor'] and i['id'] != j['id']:
                 repeat_investors.remove(j)
         investors.append(i)
-    global_statistics = {'total_investor':total_investor,'real_investor': len(investors),'global_amount_invested':global_amount_invested['amount__sum'],'total_projects':total_projects}
+    global_statistics = {'total_investor':total_investor,'real_investor': len(investors),'global_amount_invested':global_amount_invested['amount__sum'],'total_projects':total_projects,'technology_projects':technology_projects,'science_projects':science_projects,'art_projects':art_projects,'reading_projects':reading_projects,'health_projects':health_projects,'music_projects':music_projects}
     return render(request, 'home.html',{'global_statistics':global_statistics})
