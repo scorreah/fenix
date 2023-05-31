e660325f92e8a3c82ef9b112fa528ceda0e83811
sochoac1 <sochoac1@eafit.edu.co>
2023-05-31 11:16:50 -0500

    Update

--- fenix/fenix/settings.py
+++ fenix/fenix/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'investor',
     'adminmanagement',
     'rewards',
+    'paypal',
 ]
 
 MIDDLEWARE = [
--- fenix/rewards/forms.py
+++ fenix/rewards/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from .models import Reward
 
-
 class RewardForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Nombre'}))
+    money_from = forms.DecimalField(widget=forms.NumberInput(attrs={'placeholder': 'Monto'}))
+    description = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Descripción'}))
+    image = forms.ImageField(widget=forms.ClearableFileInput(attrs={'placeholder': 'Imagen'}))
+
     class Meta:
         model = Reward
         fields = ['name', 'money_from', 'description', 'image']
